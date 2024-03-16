c7e5d30a8e3892dd9e9372a2da0f261e23819418
Marcin Zepp <nircek-2103@protonmail.com>
2024-03-16 18:35:09 +0200

    Add nothing

--- biedrwise/ocr/reader.py
+++ biedrwise/ocr/reader.py
@@ -2,26 +2,31 @@ import cv2
 import pytesseract
 from pdf2image import convert_from_path
 from sys import stderr
+from pathlib import Path
+
 
 def is_number(line) -> bool:
     return line.replace(",", "").isdigit()
+
 
 def pdf_to_img(filename):
     img = convert_from_path(filename, 500)
     path = f"{filename}.png"
     img[0].save(path, "PNG")
+    assert Path(path).is_file()
     return path
 
 
 def ocr_read(filenane: str, pdf: bool) -> dict:
     if pdf:
         filenane = pdf_to_img(filenane)[0]
-    image = cv2.imread(filenane, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(filenane, cv2.IMREAD_GRAYSCALE)  # FIXME: imread returns None
+    print(image, file=stderr)
     extracted_text = pytesseract.image_to_string(image, lang="pol", config="--psm 6")
     # print(extracted_text)
     idx = extracted_text.find("Wartość")
     idx_end = extracted_text.find("Sprzedaż")
-    new_text = extracted_text[idx+8:idx_end]
+    new_text = extracted_text[idx + 8 : idx_end]
     # print(new_text)
     lines = new_text.split("\n")
     # print(lines)
@@ -30,10 +35,14 @@ def ocr_read(filenane: str, pdf: bool) -> dict:
         if "Rabat" in line or is_number(line) or not len(line):
             continue
 
-        if idx < len(lines)-1 and "Rabat" in lines[idx+1] and is_number(lines[idx+2]):
+        if (
+            idx < len(lines) - 1
+            and "Rabat" in lines[idx + 1]
+            and is_number(lines[idx + 2])
+        ):
             product_name = line.split(" ")[0]
             cnt = line.split(" ")[-4]
-            price = lines[idx+2].replace(",", ".")
+            price = lines[idx + 2].replace(",", ".")
             print(product_name, price, cnt)
             if product_name in res:
                 new_price = res[product_name][0] + float(price)
@@ -52,5 +61,5 @@ def ocr_read(filenane: str, pdf: bool) -> dict:
                 res[product_name] = (new_price, new_cnt)
             else:
                 res[product_name] = (float(price), float(cnt))
-    # print(res)            
+    # print(res)
     return res

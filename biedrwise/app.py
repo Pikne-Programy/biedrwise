b2241afc2f95b78c91443659b6b5dd9786dbee9f
Griger5 <griger04@gmail.com>
2024-03-16 14:53:10 +0100

    added user debts page

--- biedrwise/app.py
+++ biedrwise/app.py
@@ -20,5 +20,23 @@ def home():
 ]
     return render_template("home.html", dataList = dataList)
 
+@app.route("/user_debts")
+def users():
+    usersList = [
+        {
+            "name":"Marcin Cpp",
+            "debt":"10.00zł"
+        },
+        {
+            "name":"Kret Kretes",
+            "debt":"21.37zł"
+        },
+        {
+            "name":"Michał Pobuta",
+            "debt":"125.00zł"
+        }
+    ]
+    return render_template("user_debts.html", usersList=usersList)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)

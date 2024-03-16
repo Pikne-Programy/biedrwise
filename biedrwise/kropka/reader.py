269fefe5bb481212adabe746729be62ba7153650
Michał <szymocha.michal@gmail.com>
2024-03-16 16:03:45 +0100

    Add cnt for ocr

--- biedrwise/kropka/reader.py
+++ biedrwise/kropka/reader.py
@@ -32,23 +32,27 @@ def read(file) -> dict:
         if "Rabat" in line or is_number(line) or not len(line):
             continue
 
-        if (
-            idx < len(lines) - 1
-            and "Rabat" in lines[idx + 1]
-            and is_number(lines[idx + 2])
-        ):
+        if idx < len(lines)-1 and "Rabat" in lines[idx+1] and is_number(lines[idx+2]):
             product_name = line.split(" ")[0]
-            price = lines[idx + 2].replace(",", ".")
+            cnt = line.split(" ")[-4]
+            price = lines[idx+2].replace(",", ".")
+            print(product_name, price, cnt)
             if product_name in res:
-                res[product_name] += float(price)
+                new_price = res[product_name][0] + float(price)
+                new_cnt = res[product_name][1] + float(cnt)
+                res[product_name] = (new_price, new_cnt)
             else:
-                res[product_name] = float(price)
+                res[product_name] = (float(price), float(cnt))
         else:
             product_name = line.split(" ")[0]
             price = line.split(" ")[-1].replace(",", ".")
+            cnt = line.split(" ")[-4]
+            print(product_name, price, cnt)
             if product_name in res:
-                res[product_name] += float(price)
+                new_price = res[product_name][0] + float(price)
+                new_cnt = res[product_name][1] + float(cnt)
+                res[product_name] = (new_price, new_cnt)
             else:
-                res[product_name] = float(price)
+                res[product_name] = (float(price), float(cnt))
     # print(res)            
     return res

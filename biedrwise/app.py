4b6c2c86e90f8a357b45565bf8cee7bd469287d9
Dominik <dominik.godek@gmail.com>
2024-03-16 13:27:39 +0100

    add support for dynamic generation of list

--- biedrwise/app.py
+++ biedrwise/app.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    dataList = [
+    {
+        "name":"Cofe",
+        "description":"Good beverage"
+    },
+    {
+        "name":"Tea",
+        "description":"Bad beverage"
+    },
+        {
+        "name":"Blech",
+        "description":"Awesome beverage (cures autism)"
+    },
+]
+    return render_template("home.html", dataList = dataList)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
